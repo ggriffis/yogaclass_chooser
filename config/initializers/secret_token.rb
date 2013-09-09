9f12fa79023c9331efac4ac112511a0aaae7442a
Ginger Griffis <gingergriffis@hotmail.com>
2013-09-09 10:05:31 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YogaclassChooser::Application.config.secret_token = '01d9b67a27f575752191533c1674e123a392df5dd5e285a6ed679ae31b81d16d8d74ddba66cbe03efea14c0a1442e43513d03eae2688879f117993cff6dbf760'
